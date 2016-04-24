04970427c5c91e4b4b246f0f6d7339a3bfd25c40
iamthtm <p_1624_n@hotmail.com>
2016-04-24 14:24:04 +0700

    backend he

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', function (req, res) {
   res.send('hello Word')
 })
 
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'ping') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 app.set('port', (process.env.PORT || 3000))
 
 app.listen(app.get('port'), function () {
