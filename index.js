90f65d513326c2c056cc0cf38b60a1632b1a64f4
iamthtm <p_1624_n@hotmail.com>
2016-05-29 12:01:25 +0700

    add

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ app.post('/webhook/', function (req, res) {
         var gettext = text.substring(4, text.length)
         console.log('number : ' + gettext)
         var space = gettext.search(' ')
-        var num1 = parseFloat(gettext.substring(0, space))
-        var num2 = parseFloat(gettext.substring(space, gettext.length))
+        var number1 = parseFloat(gettext.substring(0, space))
+        var number2 = parseFloat(gettext.substring(space, gettext.length))
         console.log('number1 : ' + num1 + ' number2 : ' + num2)
-        var sum = num1+num2
+        var sum = number1+number2
         console.log('sum : ' + sum)
         sendTextMessage(sender, 'sum: ' + sum)
       }
